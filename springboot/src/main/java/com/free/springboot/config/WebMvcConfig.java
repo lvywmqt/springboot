01a77292b7ec79cd985988feafaec79600eaf6d3
Your Name <you@example.com>
2018-09-14 16:15:43 +0800

    1

--- springboot/src/main/java/com/free/springboot/config/WebMvcConfig.java
+++ springboot/src/main/java/com/free/springboot/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.free.springboot.config;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -8,6 +9,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
@@ -91,5 +93,12 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter implements Application
         }
         factory.setLocation(location);
         return factory.createMultipartConfig();
+	}
+	
+	/**
+	 * Bean util
+	 */
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 }
